1897c9427275744bc9494a2fd640c3b01982ad48
Oussama Gammoudi <md.oussama@gmail.com>
2015-05-22 16:06:04 +0100

    indentation fixed

--- src/msg.rs
+++ src/msg.rs
@@ -9,7 +9,7 @@ use std::ffi::CString;
 
 pub trait Message {	
 	fn encode(&mut self,&mut Write){
-		panic!("Sorry, not implemented yet");
+		unimplemented!();
 	}
 	fn len(&self)->i32 {0i32}
 }
@@ -25,31 +25,31 @@ impl WriteMessage for Write {
 
 #[derive(Clone)]
 pub enum OpCode {
-    OP_REPLY = 1 ,
-    OP_UPDATE = 2001,
-    OP_INSERT = 2002,
-    RESERVED = 2003,
-    OP_QUERY = 2004,
-    OP_GET_MORE = 2005,
-    OP_DELETE = 2006,
-    OP_KILL_CURSORS = 2007,
+	OP_REPLY = 1 ,
+	OP_UPDATE = 2001,
+	OP_INSERT = 2002,
+	RESERVED = 2003,
+	OP_QUERY = 2004,
+	OP_GET_MORE = 2005,
+	OP_DELETE = 2006,
+	OP_KILL_CURSORS = 2007,
 }
 impl OpCode {
 	pub fn from_i32(n:i32)-> OpCode {
 		match (n) {
-		        1 => OpCode::OP_REPLY,
-		        2001 => OpCode::OP_UPDATE,
-		        _ => OpCode::OP_KILL_CURSORS
+				1 => OpCode::OP_REPLY,
+				2001 => OpCode::OP_UPDATE,
+				_ => OpCode::OP_KILL_CURSORS
 		}
 	}
 }
 
 #[derive(Clone)]
 pub struct MsgHeader {
-    len : i32,      
-    id : i32,      
-    resp_to : i32,  
-    opcode : OpCode,
+	len : i32,	  
+	id : i32,	  
+	resp_to : i32,  
+	opcode : OpCode,
 }
 impl MsgHeader {
 	pub fn new(opcode:OpCode)->MsgHeader {
@@ -84,27 +84,27 @@ impl MsgHeader {
 			opcode: {
 				let opcode = buf.read_i32::<LittleEndian>().unwrap();
 				OpCode::from_i32(opcode)
-				},
+			},
 		}
 	}
 }
 
-pub struct OpGetMore {                    
-        header : MsgHeader,
-        reserved_bits : i32,
-        name_space : CString,
-        nret : i32,
-        cursor_id : i64,
+pub struct OpGetMore {					
+		header : MsgHeader,
+		reserved_bits : i32,
+		name_space : CString,
+		nret : i32,
+		cursor_id : i64,
 }
 impl OpGetMore {
 	pub fn new(name_space:&str,cursor_id:i64,nret:i32) ->OpGetMore {	 
 		let mut msg = OpGetMore {
 			header: MsgHeader::new(OpCode::OP_GET_MORE),
-	        reserved_bits : 0,
-	        name_space :CString::new(name_space).unwrap(),
-	        nret : nret,
-	        cursor_id : cursor_id,
-        };
+			reserved_bits : 0,
+			name_space :CString::new(name_space).unwrap(),
+			nret : nret,
+			cursor_id : cursor_id,
+		};
 		let msg_len = msg.len();
 		msg.header.set_len(msg_len);
 		msg
@@ -113,40 +113,40 @@ impl OpGetMore {
 impl  Message for OpGetMore {
 	fn encode(&mut self,buf:&mut Write){
 		self.header.write_to(buf);
-        let _ = buf.write_i32::<LittleEndian>(self.reserved_bits); 
-        let _ = buf.write(self.name_space.as_bytes()); 
-        let _ = buf.write(&[0u8]); // null-terminate name
-        let _ = buf.write_i32::<LittleEndian>(self.nret); 
-        let _ = buf.write_i64::<LittleEndian>(self.cursor_id); 
+		let _ = buf.write_i32::<LittleEndian>(self.reserved_bits); 
+		let _ = buf.write(self.name_space.as_bytes()); 
+		let _ = buf.write(&[0u8]); // null-terminate name
+		let _ = buf.write_i32::<LittleEndian>(self.nret); 
+		let _ = buf.write_i64::<LittleEndian>(self.cursor_id); 
 	}
 	fn len(&self)->i32{
 		(16+17+self.name_space.as_bytes().len()) as i32
 	}
 }
-                
+				
 
-pub struct OpInsert {                              // gets no response
-        header : MsgHeader,
-        flags : i32,
-        name_space : CString,
-        docs : Vec<u8>,
+pub struct OpInsert {							  // gets no response
+		header : MsgHeader,
+		flags : i32,
+		name_space : CString,
+		docs : Vec<u8>,
 }
 impl OpInsert {
 	pub fn new(name_space:&str,docs:&Vec<&Document>) ->OpInsert {
 		let mut docs_bytes = Vec::<u8>::new();
-        {
-	       let mut encoder = Encoder::new(&mut docs_bytes);
+		{
+		   let mut encoder = Encoder::new(&mut docs_bytes);
 		   for doc in docs {
 		   		let _ = encoder.encode_document(doc);
 		   }
-	    }
+		}
 			 
 		let mut msg = OpInsert {
 			header: MsgHeader::new(OpCode::OP_INSERT),
-	        flags : 0,
-	        name_space :CString::new(name_space).unwrap(),
-	        docs : docs_bytes,
-        };
+			flags : 0,
+			name_space :CString::new(name_space).unwrap(),
+			docs : docs_bytes,
+		};
 		let msg_len = msg.len();
 		msg.header.set_len(msg_len);
 		msg
@@ -158,10 +158,10 @@ impl OpInsert {
 impl  Message for OpInsert {
 	fn encode(&mut self,buf:&mut Write){
 		self.header.write_to(buf);
-        let _ = buf.write_i32::<LittleEndian>(self.flags); 
-        let _ = buf.write(self.name_space.as_bytes()); 
-        let _ = buf.write(&[0u8]); // null-terminate name
-        let _ = buf.write(&self.docs);
+		let _ = buf.write_i32::<LittleEndian>(self.flags); 
+		let _ = buf.write(self.name_space.as_bytes()); 
+		let _ = buf.write(&[0u8]); // null-terminate name
+		let _ = buf.write(&self.docs);
 	}
 	fn len(&self)->i32{
 		(16+9+self.name_space.as_bytes().len()+self.docs.len()) as i32
@@ -172,34 +172,34 @@ impl  Message for OpInsert {
 
 pub struct OpUpdate {  
 	header : MsgHeader,
-    reserved_bits : i32,
-    name_space : CString,
-    flags : i32,
-    selector : Vec<u8>,
-    update_ops : Vec<u8>,
+	reserved_bits : i32,
+	name_space : CString,
+	flags : i32,
+	selector : Vec<u8>,
+	update_ops : Vec<u8>,
 }
 impl OpUpdate {
 	
 	pub fn new(name_space:&str,selector:&Document,update_ops:&Document) ->OpUpdate {
 		let mut selector_bytes = Vec::<u8>::new();
-        {
-	       let mut encoder = Encoder::new(&mut selector_bytes);
+		{
+		   let mut encoder = Encoder::new(&mut selector_bytes);
 		   let _ = encoder.encode_document(&selector);
-	    }
-        let mut update_ops_bytes = Vec::<u8>::new();
-        {
-	       let mut encoder = Encoder::new(&mut update_ops_bytes);
+		}
+		let mut update_ops_bytes = Vec::<u8>::new();
+		{
+		   let mut encoder = Encoder::new(&mut update_ops_bytes);
 		   let _ = encoder.encode_document(&update_ops);
-	    }
+		}
 			 
 		let mut msg = OpUpdate {
 			header: MsgHeader::new(OpCode::OP_UPDATE),
-	        reserved_bits : 0,
-	        name_space :CString::new(name_space).unwrap(),
-	        flags : 0,
-	        selector : selector_bytes,
-	        update_ops : update_ops_bytes,
-        };
+			reserved_bits : 0,
+			name_space :CString::new(name_space).unwrap(),
+			flags : 0,
+			selector : selector_bytes,
+			update_ops : update_ops_bytes,
+		};
 		let msg_len = msg.len();
 		msg.header.set_len(msg_len);
 		msg
@@ -212,11 +212,11 @@ impl OpUpdate {
 impl  Message for OpUpdate {
 	fn encode(&mut self,buf:&mut Write){
 		self.header.write_to(buf);
-        let _ = buf.write_i32::<LittleEndian>(self.reserved_bits); 
-        let _ = buf.write(self.name_space.as_bytes()); 
-        let _ = buf.write(&[0u8]); // null-terminate name
-        let _ = buf.write_i32::<LittleEndian>(self.flags); 
-        let _ = buf.write(&self.selector);
+		let _ = buf.write_i32::<LittleEndian>(self.reserved_bits); 
+		let _ = buf.write(self.name_space.as_bytes()); 
+		let _ = buf.write(&[0u8]); // null-terminate name
+		let _ = buf.write_i32::<LittleEndian>(self.flags); 
+		let _ = buf.write(&self.selector);
 		let _ = buf.write(&self.update_ops);
 	}
 	fn len(&self)->i32{
@@ -226,27 +226,27 @@ impl  Message for OpUpdate {
 
 
 
-pub struct OpDelete {                              // gets no response
-    header : MsgHeader,
-    reserved_bits : i32,
-    name_space : CString,
-    flags : i32,
-    selector : Vec<u8>,
+pub struct OpDelete {							  // gets no response
+	header : MsgHeader,
+	reserved_bits : i32,
+	name_space : CString,
+	flags : i32,
+	selector : Vec<u8>,
 }
 impl OpDelete {
 	pub fn new(name_space:&str,selector:&Document) ->OpDelete {
 		let mut selector_bytes = Vec::<u8>::new();
-        {
-	       let mut encoder = Encoder::new(&mut selector_bytes);
+		{
+		   let mut encoder = Encoder::new(&mut selector_bytes);
 		   let _ = encoder.encode_document(&selector);
-	    }
+		}
 		let mut msg = OpDelete {
 			header: MsgHeader::new(OpCode::OP_DELETE),
-	        reserved_bits : 0,
-	        name_space :CString::new(name_space).unwrap(),
-	        flags : 0,
-	        selector : selector_bytes,
-        };
+			reserved_bits : 0,
+			name_space :CString::new(name_space).unwrap(),
+			flags : 0,
+			selector : selector_bytes,
+		};
 		let msg_len = msg.len();
 		msg.header.set_len(msg_len);
 		msg
@@ -255,11 +255,11 @@ impl OpDelete {
 impl Message for OpDelete {
 	fn encode(&mut self,buf:&mut Write){
 		self.header.write_to(buf);
-        let _ = buf.write_i32::<LittleEndian>(self.reserved_bits); 
-        let _ = buf.write(self.name_space.as_bytes()); 
-        let _ = buf.write(&[0u8]); // null-terminate name
-        let _ = buf.write_i32::<LittleEndian>(self.flags); 
-        let _ = buf.write(&self.selector);
+		let _ = buf.write_i32::<LittleEndian>(self.reserved_bits); 
+		let _ = buf.write(self.name_space.as_bytes()); 
+		let _ = buf.write(&[0u8]); // null-terminate name
+		let _ = buf.write_i32::<LittleEndian>(self.flags); 
+		let _ = buf.write(&self.selector);
 	}
 	fn len(&self)->i32{
 		(16+9+self.name_space.as_bytes().len()+self.selector.len()) as i32
@@ -269,20 +269,20 @@ impl Message for OpDelete {
 
 
 pub struct OpKillCursors {
-        header : MsgHeader,
-        reserved_bits : i32,
-        ncursor_ids : i32,
-        cursor_ids : Vec<i64>
+		header : MsgHeader,
+		reserved_bits : i32,
+		ncursor_ids : i32,
+		cursor_ids : Vec<i64>
 }
 impl OpKillCursors {
 	pub fn new(cursor_ids : Vec<i64>) ->OpKillCursors {
 		let len = 16 + 4*3 + 8*cursor_ids.len();
 		let mut msg = OpKillCursors {
 			header: MsgHeader::new(OpCode::OP_KILL_CURSORS),
-	        reserved_bits : 0,
-	        ncursor_ids :cursor_ids.len() as i32,
-	        cursor_ids : cursor_ids,
-        };
+			reserved_bits : 0,
+			ncursor_ids :cursor_ids.len() as i32,
+			cursor_ids : cursor_ids,
+		};
 		msg.header.set_len(len as i32);
 		msg
 	}
@@ -293,53 +293,53 @@ impl OpKillCursors {
 impl Message for OpKillCursors {
 	fn encode(&mut self,buf:&mut Write){
 		self.header.write_to(buf);
-        let _ = buf.write_i32::<LittleEndian>(self.reserved_bits); 
-        let _ = buf.write_i32::<LittleEndian>(self.ncursor_ids); 
-        for id in &self.cursor_ids {
-        	let _ = buf.write_i64::<LittleEndian>(*id);
-        };
+		let _ = buf.write_i32::<LittleEndian>(self.reserved_bits); 
+		let _ = buf.write_i32::<LittleEndian>(self.ncursor_ids); 
+		for id in &self.cursor_ids {
+			let _ = buf.write_i64::<LittleEndian>(*id);
+		};
 	}
 }
 
 
 
 
-pub struct OpQuery {                         
-    header : MsgHeader,
-    flags : i32,
-    name_space : CString,
-    nskip : i32,
-    nret : i32,
-    query : Vec<u8>,
-    ret_field_selector : Option<Vec<u8>>,
+pub struct OpQuery {						 
+	header : MsgHeader,
+	flags : i32,
+	name_space : CString,
+	nskip : i32,
+	nret : i32,
+	query : Vec<u8>,
+	ret_field_selector : Option<Vec<u8>>,
 }
 impl OpQuery {
 	pub fn new(name_space:&str,query:&Document,ret_field_selector: Option<&Document>) ->OpQuery {
 		let mut query_bytes = Vec::<u8>::new();
-        {
-	       let mut encoder = Encoder::new(&mut query_bytes);
+		{
+		   let mut encoder = Encoder::new(&mut query_bytes);
 		   let _ = encoder.encode_document(&query);
-	    }
-        let ret_field_selector_bytes  = match ret_field_selector {
-        	Some(selector) => {
-        		let mut bytes = Vec::<u8>::new();
-		        {
-			       let mut encoder = Encoder::new(&mut bytes);
+		}
+		let ret_field_selector_bytes  = match ret_field_selector {
+			Some(selector) => {
+				let mut bytes = Vec::<u8>::new();
+				{
+				   let mut encoder = Encoder::new(&mut bytes);
 				   let _ = encoder.encode_document(&selector);
-			    }
-        		Some(bytes)
-        	}
-        	None => None,
-        };
+				}
+				Some(bytes)
+			}
+			None => None,
+		};
 		let mut msg = OpQuery {
 			header: MsgHeader::new(OpCode::OP_QUERY),
-	        flags : 0,
-	        name_space :CString::new(name_space).unwrap(),
-	        nskip : 0,
-        	nret : 0,
-	        query : query_bytes,
-	        ret_field_selector : ret_field_selector_bytes,
-        };
+			flags : 0,
+			name_space :CString::new(name_space).unwrap(),
+			nskip : 0,
+			nret : 0,
+			query : query_bytes,
+			ret_field_selector : ret_field_selector_bytes,
+		};
 		
 		let msg_len = msg.len();
 		msg.header.set_len(msg_len);
@@ -352,18 +352,18 @@ impl OpQuery {
 impl  Message for OpQuery {
 	fn encode(&mut self,buf:&mut Write){
 		self.header.write_to(buf);
-        let _ = buf.write_i32::<LittleEndian>(self.flags); 
-        let _ = buf.write(self.name_space.as_bytes()); 
-        let _ = buf.write(&[0u8]); // null-terminate name
-        let _ = buf.write_i32::<LittleEndian>(self.nskip); 
-        let _ = buf.write_i32::<LittleEndian>(self.nret); 
-        let _ = buf.write(&self.query);
-	    match &self.ret_field_selector {
-	        &Some(ref selector) => { 
-	        		let _ = buf.write(&selector); 
-	        	}
+		let _ = buf.write_i32::<LittleEndian>(self.flags); 
+		let _ = buf.write(self.name_space.as_bytes()); 
+		let _ = buf.write(&[0u8]); // null-terminate name
+		let _ = buf.write_i32::<LittleEndian>(self.nskip); 
+		let _ = buf.write_i32::<LittleEndian>(self.nret); 
+		let _ = buf.write(&self.query);
+		match &self.ret_field_selector {
+			&Some(ref selector) => { 
+				let _ = buf.write(&selector); 
+			}
 			&None => (),
-	    };
+		};
 	}
 	fn len(&self)->i32{
 		let rfs_len = match &self.ret_field_selector{
@@ -377,12 +377,12 @@ impl  Message for OpQuery {
 
 
 pub struct OpReply {
-    header : MsgHeader,
-    flags : i32,
-    cursor_id : i64,
-    start : i32,
-    nret : i32,
-    pub docs : Vec<Document>,
+	header : MsgHeader,
+	flags : i32,
+	cursor_id : i64,
+	start : i32,
+	nret : i32,
+	pub docs : Vec<Document>,
 }
 impl OpReply {
 	pub fn decode(buf:&mut Read)-> OpReply{
